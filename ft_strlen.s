9e36de6bf7bc485c3217caa189f8e5168c957e8a
Semiha Beyazkilic <seozcan@student.42.fr>
2024-09-12 13:22:33 +0200

    chore: Add ft_strlen.s, .gitignore, and .img/busstructure.gif

---
+++ ft_strlen.s
@@ -0,0 +1,12 @@
+global _ft_strlen
+
+section.data	;memoire globale
+				;donnees initialises
+
+section.bss		;memoire globale
+				;donnees non initialises
+
+section.text	;memoire de programme
+				;code assembleur
+
+_ft_strlen:
